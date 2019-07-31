de0dc19b3861faf341c7e8b4d5e49c145f024f09
karl oficina <karl@aspire.com>
2019-07-30 23:24:06 -0400

    colocadas las direcciones correctas a la barra de navegacion, y la pagina/link que faltaba: nosotros

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def contacto
   end
+
+  def nosotros
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/cursos'
   get 'static_pages/tarifas'
   get 'static_pages/contacto'
+  get 'static_pages/nosotros'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
