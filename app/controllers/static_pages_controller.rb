c86bdd28ab20bf21ad734cb81c5559694ace49a2
karl oficina <karl@aspire.com>
2019-08-23 21:33:39 -0400

    agregada separacion entre secciones de inicio, en static_pages_controller se crea la informacion de la barra de navegacion y se setea cual es la pagina activa

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,126 @@
 class StaticPagesController < ApplicationController
   def inicio
+    @navegacion = [
+      {
+        direccion: static_pages_cursos_path,
+        caption: "Cursos",
+        estado: ""
+      },
+      {
+        direccion: static_pages_tarifas_path,
+        caption: "Tarifas",
+        estado: ""
+      },
+      {
+        direccion: static_pages_contacto_path,
+        caption: "Contacto",
+        estado: ""
+      },
+      {
+        direccion: static_pages_nosotros_path,
+        caption: "Nosotros",
+        estado: ""
+      },
+    ]
   end
 
   def cursos
+    @navegacion = [
+      {
+        direccion: static_pages_cursos_path,
+        caption: "Cursos",
+        estado: "active"
+      },
+      {
+        direccion: static_pages_tarifas_path,
+        caption: "Tarifas",
+        estado: ""
+      },
+      {
+        direccion: static_pages_contacto_path,
+        caption: "Contacto",
+        estado: ""
+      },
+      {
+        direccion: static_pages_nosotros_path,
+        caption: "Nosotros",
+        estado: ""
+      },
+    ]
   end
 
   def tarifas
+    @navegacion = [
+      {
+        direccion: static_pages_cursos_path,
+        caption: "Cursos",
+        estado: ""
+      },
+      {
+        direccion: static_pages_tarifas_path,
+        caption: "Tarifas",
+        estado: "active"
+      },
+      {
+        direccion: static_pages_contacto_path,
+        caption: "Contacto",
+        estado: ""
+      },
+      {
+        direccion: static_pages_nosotros_path,
+        caption: "Nosotros",
+        estado: ""
+      },
+    ]
   end
 
   def contacto
+    @navegacion = [
+      {
+        direccion: static_pages_cursos_path,
+        caption: "Cursos",
+        estado: ""
+      },
+      {
+        direccion: static_pages_tarifas_path,
+        caption: "Tarifas",
+        estado: ""
+      },
+      {
+        direccion: static_pages_contacto_path,
+        caption: "Contacto",
+        estado: "active"
+      },
+      {
+        direccion: static_pages_nosotros_path,
+        caption: "Nosotros",
+        estado: ""
+      },
+    ]
   end
 
   def nosotros
+    @navegacion = [
+      {
+        direccion: static_pages_cursos_path,
+        caption: "Cursos",
+        estado: ""
+      },
+      {
+        direccion: static_pages_tarifas_path,
+        caption: "Tarifas",
+        estado: ""
+      },
+      {
+        direccion: static_pages_contacto_path,
+        caption: "Contacto",
+        estado: ""
+      },
+      {
+        direccion: static_pages_nosotros_path,
+        caption: "Nosotros",
+        estado: "active"
+      },
+    ]
   end
 end
